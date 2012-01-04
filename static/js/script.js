5f7efcb2d81fdef57d766640d7c41e165eb3b000
rob <rob@openedit.org>
2012-01-04 11:14:23 -0500

    Static website content

---
+++ static/js/script.js
@@ -0,0 +1,31 @@
+$(window).load(function() {
+				$('.flexslider').flexslider({
+				  animation: "slide",
+				  controlsContainer: ".flex-container",
+				  slideshow: "true",
+				  slideshowSpeed: 6000 
+			  });
+			});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
